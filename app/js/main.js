2515ba8b605c9d910e29d805c8dbf7cb21dbb311
Michael Holub <michael.holub@gmail.com>
2014-07-01 05:14:55 +0400

    fps, smoother camera

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,17 @@ require({
     shim: {
      'vendor/three': { exports: 'THREE' },
      'vendor/threex.windowresize': { exports: 'THREEx' },
-     'vendor/FlyControls': { exports: 'THREE' },
+     'vendor/FlyControls': {
+        deps: ['vendor/three'],
+        exports: 'THREE' },
+     'vendor/stats': { exports: 'Stats' }
     }
 }, [
-    'vendor/three', 'vendor/threex.windowresize', 'vendor/FlyControls'
-], function(THREE, THREEx) {
+    'vendor/three', 'vendor/threex.windowresize', 'vendor/stats', 'vendor/FlyControls'
+], function(THREE, THREEx, Stats) {
 
-var scene, renderer, winResize, controls;
+var scene, renderer;
+var winResize, controls, stats;
 var camera, cameraParent;
 var geometry, material, mesh;
 
@@ -23,10 +27,16 @@ function init() {
     renderer = new THREE.WebGLRenderer( {
         antialias: true
     } );
+    renderer.setClearColor(0xffe300, 1);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    renderer.setClearColor(0xffe300, 1);
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.bottom = '0px';
+    stats.domElement.style.zIndex = 100;
+    document.body.appendChild(stats.domElement);
+
 
     cameraParent = new THREE.Object3D();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
@@ -36,15 +46,15 @@ function init() {
 
     controls = new THREE.FlyControls( cameraParent, renderer.domElement );
     controls.movementSpeed = 0;
-    controls.rollSpeed = 0.05;
+    controls.rollSpeed = 0.2;
     
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(1, 1, 1);
+    geometry = new THREE.TorusGeometry(1, 0.2, 16, 32);
     material = new THREE.MeshBasicMaterial({
         color: 0x000000,
         wireframe: true,
-        wireframeLinewidth: 3
+        wireframeLinewidth: 2
     });
 
     mesh = new THREE.Mesh(geometry, material);
@@ -52,23 +62,36 @@ function init() {
     scene.add(cameraParent);
 }
 
-function limitControls() {
-    var l = 0.2;
-    cameraParent.rotation.x = Math.max(-l, Math.min(l, cameraParent.rotation.x));
-    cameraParent.rotation.y = Math.max(-l, Math.min(l, cameraParent.rotation.y));
-    cameraParent.rotation.z = 0;
+// hard limit
+function limitControls(target) {
+    var l = 0.1;
+    var k = 0.98;
+    target.rotation.z = 0;
+
+    if (Math.abs(target.rotation.x) > l) {
+        target.rotation.x = k * target.rotation.x + (1 - k) * THREE.Math.sign(target.rotation.x) * l;
+    }
+    if (Math.abs(target.rotation.y) > l) {
+        target.rotation.y = k * target.rotation.y + (1 - k) * THREE.Math.sign(target.rotation.y) * l;
+    }
+    // target.rotation.x = THREE.Math.clamp(target.rotation.x, -l, l);
+    // target.rotation.y = THREE.Math.clamp(target.rotation.y, -l, l);
 }
 
 function animate() {
+    stats.begin();
+
     var delta = clock.getDelta();
     requestAnimationFrame(animate);
 
     // mesh.rotation.x += 0.01;
     // mesh.rotation.y += 0.02;
     controls.update( delta );
-    limitControls();
+    limitControls(cameraParent, controls);
 
     renderer.render(scene, camera);
+
+    stats.end();
 }
 
 });
